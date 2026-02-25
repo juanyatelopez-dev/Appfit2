b523a052bd82733d6bba1d0afea39e8007cb137e
juanyatelopez-dev <juanyatelopez@gmail.com>
2026-02-25 17:50:40 -0500

    alpha 1.0.11 autentificación

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,10 +7,18 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import AppRoutes from "@/routes/AppRoutes";
 import RouteIndicator from "@/components/RouteIndicator";
+import { supabase } from "@/services/supabaseClient";
 
 const queryClient = new QueryClient();
 
 const App = () => {
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      console.log("Session after redirect:", data.session);
+    });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
